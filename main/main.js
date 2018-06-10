8a31a47d22b1a84a8211b05cb90f9a3822fbf201
DingGe520R <784872490@qq.com>
2018-06-10 15:38:18 +0800

    4

--- main/main.js
+++ main/main.js
@@ -1,6 +1,24 @@
 // Write your cade below:
-function main() {};
 
-module.exports = {
-    main
-}
+var main=function main(number) {
+   
+    var input = "1234";
+    var A = 0, B = 0;
+    for (let i in input) {
+        for (let j in number) {
+            if (input[i] === number[j] && i === j) {
+                A++;
+            }
+            if (input[i] === number[j] && i != j) {
+                B++;
+            }
+        }
+    }
+    return (A + 'A' + B + 'B');
+ 
+ };
+
+module.exports =  main
+
+   
+
